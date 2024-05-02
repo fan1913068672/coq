2c09357de5f33d535593d653ba17f36921ea3102
fyh <1913068672@qq.com>
2024-05-03 02:04:58 +0800

    add prefix

--- build_install.sh
+++ build_install.sh
@@ -2,7 +2,7 @@
 
 ## build and install precedure
 make clean
-./configure
+./configure -prefix "$(pwd)/_install_ci"
 make dunestrap
 dune build -p coq-core,coq-stdlib,coq,coqide-server,coqide
-dune install coq-core coq-stdlib coq coqide-server coqide
+dune install --prefix="$(pwd)/_install_ci" coq-core coq-stdlib coq coqide-server coqide
